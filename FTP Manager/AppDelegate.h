01a74e995688bb9d9f57e8ce553ee3af51567b0b
admin <admin@ios-developer-2.local>
2017-03-27 11:50:07 +0530

    Initial Commit

---
+++ FTP Manager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FTP Manager
+//
+//  Created by admin on 27/03/17.
+//  Copyright © 2017 VK11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FTP Manager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FTP Manager
+//
+//  Created by admin on 27/03/17.
+//  Copyright © 2017 VK11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
